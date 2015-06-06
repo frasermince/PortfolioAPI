03415065262f501f2edf82b51a5fe5a1f4b6bbaf
Fraser <frasermince@gmail.com>
2015-06-06 14:40:18 -0500

    add get method and main

--- server.hs
+++ server.hs
@@ -8,19 +8,23 @@ import Data.Aeson
 import Data.List
 import GHC.Generics
 import Network.Wai
+import Network.Wai.Handler.Warp
 import Servant
+import Data.Text
 
 type Name = Text
 
 type PortfolioAPI =
-  "presentation" :> ReqBody `[JSON] Name :> ReqBody '[JSON] [Annotation] :> Post '[JSON] [Presentation]
+  "presentation" :> ReqBody '[JSON] Name :> ReqBody '[JSON] [Annotation] :> Post '[JSON] Presentation
+  :<|> "presentation" :> Capture "presentationID" Integer :> Get '[JSON] Presentation
 
 data File = File
- { path     :: text
- , contents :: text
+ { path     :: Text
+ , contents :: Text
  } deriving (Eq, Show, Generic)
 
 instance FromJSON File
+instance ToJSON File
 
 data Annotation = Annotation
  { file       :: File
@@ -29,6 +33,7 @@ data Annotation = Annotation
  } deriving (Eq, Show, Generic)
 
 instance FromJSON Annotation
+instance ToJSON Annotation
 
 data Presentation = Presentation
  { annotations :: [Annotation]
@@ -36,14 +41,39 @@ data Presentation = Presentation
  } deriving (Eq, Show, Generic)
 
 instance FromJSON Presentation
+instance ToJSON Presentation
+
+presentation = Presentation
+               { annotations = [a1, a2]
+               , name        = "name"
+               }
+  where file = File
+               { path     = "THIS IS A PATH"
+               , contents = "CONTENTS OF A FILE"
+               }
+        a1 = Annotation
+              { file       = file
+             , lineNumber = 5
+             , comment    = "This is a comment"
+             }
+        a2 = Annotation
+             { file       = file
+             , lineNumber = 7
+             , comment    = "This is another comment"
+             }
 
 api :: Proxy PortfolioAPI
 api = Proxy
 
 server :: Server PortfolioAPI
-server = presentation
-  where presentation :: Name -> [Annotation] -> EitherT ServantErr IO Presentation
-        presenation name annotations = return (Presentation name annotations)
+server = create :<|> show
+  where create :: Name -> [Annotation] -> EitherT ServantErr IO Presentation
+        create name annotations = return Presentation {name = name, annotations = annotations}
+
+        show :: Integer -> EitherT ServantErr IO Presentation
+        show id = return presentation
 
 app :: Application
 app = serve api server
+
+main = run 8080 app
